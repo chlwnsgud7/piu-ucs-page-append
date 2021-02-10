b4fb25234ebe7c982b75590bf97b073507979d2b
Envil_Saintan <chlwnsgud7@gmail.com>
2021-02-11 05:27:17 +0900

    chrome.tabs.excuteScript

--- source/popup.js
+++ source/popup.js
@@ -1,79 +1,94 @@
 const MYUCS_PAGE = "http://www.piugame.com/piu.ucs/ucs.my_ucs/ucs.my_upload.php";
 
-function promise_current_ucs_numbers() {
-    return new Promise( )
+async function promise_current_ucs_numbers() {
+    try {
+        const response = await fetch(MYUCS_PAGE);
+        const html = await response.text();
 
-
-    fetch(MYUCS_PAGE).then((response) => {
-        return response.text();
-    }).then((html) => {
         let parser = new DOMParser();
         let doc = parser.parseFromString(html, 'text/html');
+        let ucs_numbers = Array();
         Array.from(doc.getElementsByClassName("ucs_slot_delete")).forEach(
             (ucs_button) => { ucs_numbers.push(ucs_button.attributes['data-ucs_no'].value); }
         );
-    }).catch((err) => {
-        console.warn('An Error Occured.', err);
-    });
 
-    return ucs_numbers;
+        return ucs_numbers;
+    } catch (err) {
+        console.warn('An Error Occured.', err);
+    }
 }
 
 function custom_add_ucs(ucs_id) {
-    $.ajax({
-        type: "GET",
-        url: "/piu.ucs/ucs.share/ucs.share.ajax.php",
-        data: {
-            "ucs_id": ucs_id,
-            "work_type": "AddtoUCSSLOT"
-        },
-        cache: false,
-        async: false
+    chrome.tabs.excuteScript({
+        code: `$.ajax({
+            type: "GET",
+            url: "/piu.ucs/ucs.share/ucs.share.ajax.php",
+            data: {
+                "ucs_id": ${ucs_id},
+                "work_type": "AddtoUCSSLOT"
+            },
+            cache: false,
+            async: false
+        });`
     });
 }
 
 function custom_delete_ucs(ucs_id) {
-    $.ajax({
-        type: "GET",
-        url: "/piu.ucs/ucs.share/ucs.share.ajax.php",
-        data: {
-            "data_no": ucs_id,
-            "work_type": "RemovetoUCSSLOT2"
-        },
-        cache: false,
-        async: false
+    chrome.tabs.excuteScript({
+        code: `$.ajax({
+            type: "GET",
+            url: "/piu.ucs/ucs.share/ucs.share.ajax.php",
+            data: {
+                "data_no": ${ucs_id},
+                "work_type": "RemovetoUCSSLOT2"
+            },
+            cache: false,
+            async: false
+        });`
     });
 }
 
 function custom_make_ucs_zip() {
-    $.ajax({
-        type: "GET",
-        url: "/piu.ucs/ucs.share/ucs.share.ajax.php",
-        data: {
-            "work_type": "MakeUCSPack"
-        },
-        cache: false,
-        async: false
+    chrome.tabs.excuteScript({
+        code: `$.ajax({
+            type: "GET",
+            url: "/piu.ucs/ucs.share/ucs.share.ajax.php",
+            data: {
+                "work_type": "MakeUCSPack"
+            },
+            cache: false,
+            async: false
+        });`
+    });
+}
+
+function build_ucs_pack() {
+    promise_current_ucs_numbers().then((ucs_numbers) => {
+        ucs_numbers.forEach((ucs_id) => { custom_delete_ucs(ucs_id) });
+        Array.from(document.getElementsByClassName("ucs_id_input")).forEach((ucs_id) => { if (ucs_id) custom_add_ucs(ucs_id); });
+        custom_make_ucs_zip();
     });
 }
 
 function init_document() {
-    let current_ucs_div = document.getElementById("current-ucs");
-    let ucs_id_inputs = Array(10);    
-    let current_ucs_numbers = get_current_ucs_numbers();
-    let len = current_ucs_numbers.length;
-    console.log(len);
+    promise_current_ucs_numbers().then((ucs_numbers) => {
+        let current_ucs_div = document.getElementById("current-ucs");
+        let ucs_id_inputs = Array(10);
 
-    for (let i = 0; i < 10; i++) {
-        ucs_id_inputs[i] = document.createElement("input");
+        for (let i = 0; i < 10; i++) {
+            ucs_id_inputs[i] = document.createElement("input");
+            current_ucs_div.appendChild(ucs_id_inputs[i]);
 
-        ucs_id_inputs[i].setAttribute("class", "ucs_id_input");
-        ucs_id_inputs[i].setAttribute("type", "number");
-        ucs_id_inputs[i].setAttribute("min", "1");
-        if (i < len) ucs_id_inputs[i].setAttribute("value", current_ucs_numbers[i]);
+            ucs_id_inputs[i].setAttribute("class", "ucs_id_input");
+            ucs_id_inputs[i].setAttribute("type", "number");
+            ucs_id_inputs[i].setAttribute("min", "1");
+            if (i < ucs_numbers.length) ucs_id_inputs[i].setAttribute("value", ucs_numbers[i]);
+        }
+    });
 
-        current_ucs_div.appendChild(ucs_id_inputs[i]);
-    }
+    let ucs_pack_build_button = document.getElementById("build-ucs-pack");
+    ucs_pack_build_button.onclick = build_ucs_pack;
 }
+
 
 init_document();
